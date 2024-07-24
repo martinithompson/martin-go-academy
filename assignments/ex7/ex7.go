022293439c3e86152f07c3d976cb2e74728177ee
Martin Thompson <martin.thompson@bjss.com>
2024-07-24 15:41:07 +0100

    ex7 continued

--- assignments/ex7/ex7.go
+++ assignments/ex7/ex7.go
@@ -19,7 +19,36 @@ func RollDice(r Randomiser) int {
 	return r.Generate(6)
 }
 
-func main() {
+func SumTwoDice() int {
 	randomiser := IntRandomiser{}
-	fmt.Println(RollDice(randomiser))
+	diceOne := RollDice(randomiser)
+	diceTwo := RollDice(randomiser)
+	return diceOne + diceTwo
+}
+
+func getRollResult(total int) string {
+	var result string
+	switch total {
+	case 7, 11:
+		result = fmt.Sprintf("Roll %d: NATURAL", total)
+	case 2:
+		result = fmt.Sprintf("Roll %d: SNAKE-EYES-CRAPS", total)
+	case 3, 12:
+		result = fmt.Sprintf("Roll %d: LOSS-CRAPS", total)
+	default:
+		result = fmt.Sprintf("Roll %d: NEUTRAL", total)
+	}
+	return result
+}
+
+func RollMultiple(count int) {
+	var total int
+	for i := 1; i <= count; i++ {
+		total = SumTwoDice()
+		fmt.Println(getRollResult(total))
+	}
+}
+
+func main() {
+	RollMultiple(50)
 }
--- assignments/ex7/ex7_test.go
+++ assignments/ex7/ex7_test.go
@@ -23,3 +23,22 @@ func TestRollDice(t *testing.T) {
 // test roll x(50) times - store in array of scores
 
 // test loop through array and output score, NATURAL etc
+
+func TestGetRollResult(t *testing.T) {
+	assertResult :=
+		func(t testing.TB, got, want string) {
+			if got != want {
+				t.Errorf("got %q want %q", got, want)
+			}
+		}
+	t.Run("snake eyes", func(t *testing.T) {
+		got := getRollResult(2)
+		want := "Roll 2: SNAKE-EYES-CRAPS"
+		assertResult(t, got, want)
+	})
+	t.Run("neutral", func(t *testing.T) {
+		got := getRollResult(4)
+		want := "Roll 4: NEUTRAL"
+		assertResult(t, got, want)
+	})
+}

a7a1df95ca6449744e7960aa247dec3d3b2ba640
Martin Thompson <martin.thompson@bjss.com>
2024-08-08 16:15:06 +0100

    attempt to add patch delete tests

--- assignments/todo-app/final/api/server_test.go
+++ assignments/todo-app/final/api/server_test.go
@@ -36,9 +36,54 @@ func TestTodosHandler(t *testing.T) {
 		assertValue(t, res[0].Completed, false)
 		assertValue(t, len(res), 1)
 	})
+
+	// PATCH and DELETE tests go into infinite loop, possible bug!
 	// t.Run("PATCH a todo", func(t *testing.T) {
+	// 	// create an initial todo
+	// 	name := "Initial Todo"
+	// 	req := addTodo(name)
+	// 	rec := httptest.NewRecorder()
+	// 	server.todosHandler(rec, req)
+
+	// 	var todosList []todos.Todo
+	// 	json.Unmarshal(rec.Body.Bytes(), &todosList)
+	// 	if len(todosList) == 0 {
+	// 		t.Fatal("expected todo to be created")
+	// 	}
+	// 	todoID := todosList[0].Id
+
+	// 	// update the todo
+	// 	patchReq := patchTodo(todoID, "Updated Todo", "true")
+	// 	rec = httptest.NewRecorder()
+	// 	server.todosHandler(rec, patchReq)
+
+	// 	assertStatusCode(t, rec.Code, http.StatusOK)
+
+	// 	var updatedTodos []todos.Todo
+	// 	json.Unmarshal(rec.Body.Bytes(), &updatedTodos)
+	// 	assertValue(t, updatedTodos[0].Name, "Updated Todo")
+	// 	assertValue(t, updatedTodos[0].Completed, true)
 	// })
 	// t.Run("DELETE a todo", func(t *testing.T) {
+	// 	// Add a new todo first
+	// 	name := "Delete me"
+	// 	req := addTodo(name)
+	// 	rec := httptest.NewRecorder()
+	// 	server.todosHandler(rec, req)
+
+	// 	var todosList []todos.Todo
+	// 	json.Unmarshal(rec.Body.Bytes(), &todosList)
+	// 	if len(todosList) == 0 {
+	// 		t.Fatal("expected todo to be created")
+	// 	}
+	// 	todoID := todosList[0].Id
+
+	// 	// delete the todo
+	// 	deleteReq := deleteTodo(todoID)
+	// 	rec = httptest.NewRecorder()
+	// 	server.todosHandler(rec, deleteReq)
+
+	// 	assertStatusCode(t, rec.Code, http.StatusNoContent)
 	// })
 }
 
@@ -63,9 +108,14 @@ func addTodo(name string) *http.Request {
 	return req
 }
 
-// func patchTodo(id, name, completed string) *http.Request {
-// 	t := map[string]string{"id": id, "name": name, "completed": completed}
-// 	j, _ := json.Marshal(t)
-// 	req, _ := http.NewRequest(http.MethodPatch, "/todos/"+id, bytes.NewBuffer(j))
-// 	return req
-// }
+func patchTodo(id, name, completed string) *http.Request {
+	t := map[string]string{"id": id, "name": name, "completed": completed}
+	j, _ := json.Marshal(t)
+	req, _ := http.NewRequest(http.MethodPatch, "/todos/"+id, bytes.NewBuffer(j))
+	return req
+}
+
+func deleteTodo(id string) *http.Request {
+	req, _ := http.NewRequest(http.MethodDelete, "/todos/"+id, nil)
+	return req
+}
